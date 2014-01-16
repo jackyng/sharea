62a48fe325ae270b82246c4eee50b89b9089665b
Kevin Tan <thektan@gmail.com>
2014-01-16 02:21:05 -0800

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116084749) do
+ActiveRecord::Schema.define(version: 20140116101702) do
+
+  create_table "expense_splits", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "expense_id"
+    t.integer  "amount_owe"
+    t.integer  "amount_pay"
+    t.boolean  "completed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "expenses", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "deadline"
+    t.integer  "owner_id"
+    t.boolean  "completed"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
